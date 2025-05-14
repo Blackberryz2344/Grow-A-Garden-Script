ddcbb626f9a3d4bc4951c1a79044c9d7898038d5
Blackberryz2344 <162956052+Blackberryz2344@users.noreply.github.com>
2025-05-14 01:02:01 -0400

    Update SeedShop.lua

--- SeedShop.lua
+++ SeedShop.lua
@@ -2,28 +2,28 @@ local gui = Instance.new("ScreenGui")
 gui.Parent = game.Players.LocalPlayer.PlayerGui
 
 local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0.2, 0, 0.1, 0)
+frame.Size = UDim2.new(10, 0, 0, 10)
 frame.Position = UDim2.new(0.4, 0, 0.4, 0)
 frame.BackgroundColor3 = Color3.new(1, 1, 1)
 frame.Parent = gui
 
 local botaoAtivar = Instance.new("TextButton")
 botaoAtivar.Text = "Ativar"
-botaoAtivar.Size = UDim2.new(0.4, 0, 0.8, 0)
-botaoAtivar.Position = UDim2.new(0.1, 0, 0.1, 0)
+botaoAtivar.Size = UDim2.new(2, 1, 2)
+botaoAtivar.Position = UDim2.new(1100, 0, 0.1, 100)
 botaoAtivar.Parent = frame
 
 local botaoDesativar = Instance.new("TextButton")
 botaoDesativar.Text = "Desativar"
-botaoDesativar.Size = UDim2.new(0.4, 0, 0.8, 0)
-botaoDesativar.Position = UDim2.new(0.5, 0, 0.1, 0)
+botaoDesativar.Size = UDim2.new(2, 1, 2)
+botaoDesativar.Position = UDim2.new(1100, 0.1, 200)
 botaoDesativar.Parent = frame
 
 local scriptAtivado = false
 
 botaoAtivar.MouseButton1Click:Connect(function()
     scriptAtivado = true
-    loadstring(game:HttpGet("https:                                                                                                 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackberryz2344/Grow-A-Garden-Script/main/SeedShopModificado.lua"))()
 end)
 
 botaoDesativar.MouseButton1Click:Connect(function()
@@ -47,15 +47,9 @@ local SeedShopModificado = [[
     end
 ]]
 
--- Salvar o script modificado em um arquivo no GitHub
--- e usar o link do arquivo no lugar do script acima
--- Exemplo:
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackberryz2344/Grow-A-Garden-Script/main/SeedShopModificado.lua"))()
-
--- Mas como estamos fazendo tudo em um script, podemos simplesmente usar o loadstring com o script acima
 local function loadSeedShopModificado()
     loadstring(SeedShopModificado)()
 end
 
 botaoAtivar.MouseButton1Click:Connect(loadSeedShopModificado)
-        
+
