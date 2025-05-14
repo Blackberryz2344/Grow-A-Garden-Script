746112e6a407a454bb33b0940ffb287b025c65ed
Blackberryz2344 <162956052+Blackberryz2344@users.noreply.github.com>
2025-05-14 00:54:29 -0400

    SeedShop.lua

--- SeedShop.lua
+++ SeedShop.lua
@@ -1,13 +1,61 @@
--- Script da loja de sementes
-function updateStock_upvr()
-    local any_GetData_result1 = DataService_upvr:GetData()
-    local var60 = 9999
-    Main_Frame_upvr.Stock_Text.Text = `X{var60} Stock`
-    if var60 > 0 then
-        Frame_upvr.Sheckles_Buy.In_Stock.Visible = true
-        Frame_upvr.Sheckles_Buy.No_Stock.Visible = false
-    else
-        Frame_upvr.Sheckles_Buy.In_Stock.Visible = false
-        Frame_upvr.Sheckles_Buy.No_Stock.Visible = true
+local gui = Instance.new("ScreenGui")
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+
+local frame = Instance.new("Frame")
+frame.Size = UDim2.new(0.2, 0, 0.1, 0)
+frame.Position = UDim2.new(0.4, 0, 0.4, 0)
+frame.BackgroundColor3 = Color3.new(1, 1, 1)
+frame.Parent = gui
+
+local botaoAtivar = Instance.new("TextButton")
+botaoAtivar.Text = "Ativar"
+botaoAtivar.Size = UDim2.new(0.4, 0, 0.8, 0)
+botaoAtivar.Position = UDim2.new(0.1, 0, 0.1, 0)
+botaoAtivar.Parent = frame
+
+local botaoDesativar = Instance.new("TextButton")
+botaoDesativar.Text = "Desativar"
+botaoDesativar.Size = UDim2.new(0.4, 0, 0.8, 0)
+botaoDesativar.Position = UDim2.new(0.5, 0, 0.1, 0)
+botaoDesativar.Parent = frame
+
+local scriptAtivado = false
+
+botaoAtivar.MouseButton1Click:Connect(function()
+    scriptAtivado = true
+    loadstring(game:HttpGet("https:                                                                                                 
+end)
+
+botaoDesativar.MouseButton1Click:Connect(function()
+    scriptAtivado = false
+    -- Código para desativar o script
+end)
+
+-- Script da loja de sementes modificado
+local SeedShopModificado = [[
+    function updateStock_upvr()
+        local any_GetData_result1 = DataService_upvr:GetData()
+        local var60 = 9999
+        Main_Frame_upvr.Stock_Text.Text = `X{var60} Stock`
+        if var60 > 0 then
+            Frame_upvr.Sheckles_Buy.In_Stock.Visible = true
+            Frame_upvr.Sheckles_Buy.No_Stock.Visible = false
+        else
+            Frame_upvr.Sheckles_Buy.In_Stock.Visible = false
+            Frame_upvr.Sheckles_Buy.No_Stock.Visible = true
+        end
     end
+]]
+
+-- Salvar o script modificado em um arquivo no GitHub
+-- e usar o link do arquivo no lugar do script acima
+-- Exemplo:
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Blackberryz2344/Grow-A-Garden-Script/main/SeedShopModificado.lua"))()
+
+-- Mas como estamos fazendo tudo em um script, podemos simplesmente usar o loadstring com o script acima
+local function loadSeedShopModificado()
+    loadstring(SeedShopModificado)()
 end
+
+botaoAtivar.MouseButton1Click:Connect(loadSeedShopModificado)
+        
